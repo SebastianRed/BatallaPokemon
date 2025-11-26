2a2d955b4822bd17f5089d230d7ee9d3507b06db
Sebastián Rojo <sebastianrojocollao@gmail.com>
2025-11-25 23:29:21 -0300

    Avance de clase

--- src/BatallaPokemon.java
+++ src/BatallaPokemon.java
@@ -1,24 +1,123 @@
+import java.util.Random;
+import java.util.Scanner;
+
 class Pokemon {
-    
-    String nombre;
-    Integer saludMax;
-    Integer saludActual;
+	String nombre;
+	int vidaMax;
+	int vidaActual;
+	boolean defendiendo;
 
-    public Pokemon(String nombre, Integer salud) {
-        this.nombre = nombre;
-        this.saludMax = salud;
-        this.saludActual = salud;
-    }
+	// Contructor
+	// pedir -> Nombre y Vida maxima
+	public Pokemon(String pokemonName, int vida) {
+		this.nombre = pokemonName;
+		this.vidaMax = vida;
+		this.vidaActual = vida;
+	}
 
+	boolean estaVivo() {
+		return vidaActual > 0;
+	}
+
+	// ---------------------------------------------------------
+	// ---------------- ACCIONES DE BATALLA --------------------
+	// ---------------------------------------------------------
+
+	void mostrarEstado() {
+		// Pikachu ♥ 90/95
+		System.out.println(nombre + " ♥ " + vidaActual + "/" + vidaMax);
+	}
+
+	void recibirDano(int dano) {
+		vidaActual -= dano;
+
+		if(vidaActual < 0) {
+			vidaActual = 0;
+		}
+
+		// .𖥔 Pikachu recibe 100 de daño.
+		System.out.println(".𖥔 " + nombre + " recibe " + dano + " de daño.");
+	}
+
+	void curar(int cantidad) {
+		vidaActual += cantidad;
+
+		if(vidaActual > vidaMax) {
+			vidaActual = vidaMax;
+		}
+
+		// +♥ Pikachu se cura 100 puntos de vida.
+		System.out.println("+♥ " + nombre + " se cura " + cantidad + " puntos de vida");
+	}
+
+	void defender() {
+		defendiendo = true;
+		// ⛊ Pikachu se pone en posicion defensiva.
+		System.out.println("⛊ " + nombre + " se pone en posicion defensiva.");
+	}
 }
 
-public class BatallaPokemon {
+public class BatallaPokemon { 
+	public static void main(String[] args) { 
 
-    public static void main(String[] args) {
-        Pokemon pikachu = new Pokemon("Pikachu", 100);
-        Pokemon charmander = new Pokemon("Charmander", 80);
+		Scanner sc = new Scanner(System.in);
+		Random random = new Random();
 
-        System.out.println("Pokemon 1: " + pikachu.nombre + " | Salud: " + pikachu.saludActual + "/" + pikachu.saludMax);
-        System.out.println("Pokemon 2: " + charmander.nombre + " | Salud: " + charmander.saludActual + "/" + charmander.saludMax);
-    }
+		// crear nuestra lista de pokemon
+		// que van a luchar
+		// String[] lista = ["A", "B", "C"];
+		// Pokemon poke1 = new Pokemon("Charmander", 100);
+		Pokemon[] lista = {
+			new Pokemon("Charmander", 100),
+			new Pokemon("Squirtle", 110),
+			new Pokemon("Bulbasaur", 105),
+			new Pokemon("Pikachu", 95)
+		};
+
+		System.out.println("");
+		System.out.println("𓃻  BATALLA POKEMON 𓅂");
+		System.out.println("Elige tu Pokemon:");
+		System.out.println("");
+
+		for(int i = 0; i < lista.length; i++){
+			// 1) Charmander [100] - HP
+			// 2) Squirtle [110] - HP
+			
+			/*
+			(i+1)    -> 0+1, para que muestre 1
+			lista[i] -> hace referencia al elemento de mi lista
+			lista[i].nombre -> atributo nombre
+			*/
+
+			System.out.println((i + 1) + ") " + lista[i].nombre + " [" + lista[i].vidaMax + "] - HP");
+		}
+
+		System.out.println("Opcion: ");
+		int opcionJugador = sc.nextInt() - 1;
+
+		if(opcionJugador < 0 || opcionJugador >= lista.length ) {
+			System.out.println("Opcion no valida, se asignara el primero de la lista");
+			opcionJugador = 0;
+		}
+
+		//                  lista[2];
+		Pokemon tuPokemon = lista[opcionJugador];
+
+		int opcionMaquina = random.nextInt(lista.length);
+
+		if(opcionMaquina == opcionJugador && lista.length > 1) {
+			//       0    =      3 + 1  = 4 / 4 = 1   (% == 0)
+			//       2    =      1 + 1  = 2 / 4 = 0.5 (% == 2)
+			opcionMaquina = (opcionMaquina + 1) % lista.length;
+		}
+
+		//Pokemon pokemonCPU = lista[opcionMaquina];
+
+		// new Pokemon(nombre, vida);
+		Pokemon pokemonCPU = new Pokemon(lista[opcionMaquina].nombre, lista[opcionMaquina].vidaMax);
+
+		System.out.println("\nTu eliges a: " + tuPokemon.nombre);
+		System.out.println("La CPU elige a: " + pokemonCPU.nombre);
+		System.out.println("\n¡Que comience la batalla!\n");
+	}
 }
